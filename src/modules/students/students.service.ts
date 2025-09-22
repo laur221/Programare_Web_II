27619c666aa717997a5b83019a9d239898afc3b0
laur221 <kridderurt@gmail.com>
2025-09-22 08:52:32 +0300

    delete unused import

--- src/modules/students/students.service.ts
+++ src/modules/students/students.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Student } from './student.entity';
-import { group } from 'console';
 
 @Injectable()
 export class StudentsService {
